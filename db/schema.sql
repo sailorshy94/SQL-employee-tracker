64cd56cf1bec30ccaeef6a884d981fc372ca6e83
sailorshy94 <sbutle94@gmail.com>
2024-01-22 18:06:28 -0500

    put data into schema.sql file to create db named business, within db creates 3 tables: department, job_titles, employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,40 @@
 /* will create tables */
+DROP DATABASE IF EXISTS business_db;
+
+CREATE DATABASE business_db;
+
+USE business_db;
+
+SELECT DATABASE();
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO-INCREMENT,
+    name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE job_titles (
+    id INT NOT NULL AUTO-INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO-INCREEMENT,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    job_title_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (job_titles_id)
+    REFERENCES job_titles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) 
+    REFERENCES (manager_id)
+    ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
