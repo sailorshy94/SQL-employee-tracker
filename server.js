df2302dd31ade97d658ed34bbd3f02b436434779
sailorshy94 <sbutle94@gmail.com>
2024-01-23 18:16:11 -0500

    created inquirer prompt module and used console.log to test input - success

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 // imports mysql
 const mysql = require('mysql2');
 
-// connects to business_db
+// connects to business_db and logs connection msg
 const db = mysql.createConnection(
     {
         user: 'root',
@@ -11,3 +11,28 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to business database.`)
 );
+
+// TODO: add prompt module
+const questions = inquirer.createPromptModule();
+// user will select which list they want to view
+questions (
+    {
+    type: 'rawlist',
+    name: 'views',
+    message: 'Select an option to view data',
+    choices: [
+        'View All Departments',
+        // 'View All Roles',
+        // 'View All Employees'
+    ]
+    // console logs response obj in terminal
+    }).then((response) => {
+        console.log(response);
+    });
+
+// TODO: create init function to wrap around prompt module so it will come up ea time
+
+//  TODO: add queries for :
+    // view all roles
+    // view all depts
+    // view all employees
