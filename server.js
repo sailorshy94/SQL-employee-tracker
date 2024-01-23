e4d15e1ceaa4651165e4863e6578a67dd451c4bc
sailorshy94 <sbutle94@gmail.com>
2024-01-23 17:24:14 -0500

    added const for db and assigned it to mysql.createConnection in order to create a connection to database, added template in for information needed within connection and console log to announce connection

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+// imports inquirer
 const inquirer = require('inquirer');
-
+// imports mysql
 const mysql = require('mysql2');
+
+// connects to business_db
+const db = mysql.createConnection(
+    {
+        host: '',
+
+        user: '',
+
+        password: '',
+
+        database: ''
+    },
+    console.log(`Connected to business database.`)
+);
