635ca018b13e8c6002f47aee3f5c692ae9afb7e7
sailorshy94 <sbutle94@gmail.com>
2024-01-23 21:13:30 -0500

    wrapped init function around prompt module so that it will auto populate questions each time an answer is selected

--- index.js
+++ index.js
@@ -12,46 +12,47 @@ const db = mysql.createConnection(
     console.log(`Connected to business database.`)
 );
 
-// TODO: add prompt module
-const questions = inquirer.createPromptModule();
-// user will select which list they want to view
-questions (
-    {
-    type: 'rawlist',
-    name: 'views',
-    message: 'Select an option to view data',
-    choices: [
-        'View All Departments',
-        'View All Roles',
-        'View All Employees'
-    ]
-    // console logs response obj in terminal
-    }).then((response) => {
-        // console.log(response);
-        if (response.views === 'View All Departments'){
-            db.query('SELECT * FROM departments', (error, departments) => {
-                if (error) console.error(error);
-                // displays query as a table in terminal
-                console.table(departments);
-            })
-        }
-        if (response.views === 'View All Roles'){
-            db.query('SELECT * FROM job_titles', (error, job_titles) => {
-                if (error) console.error(error);
-                console.table(job_titles);
-            })
-        }
-        if (response.views === 'View All Employees'){
-            db.query('SELECT * FROM employees', (error, employees) => {
-                if (error) console.error(error);
-                console.table(employees);
-            })
-        }
-    });
-
 // TODO: create init function to wrap around prompt module so it will come up ea time
+const init = () => {
+    const questions = inquirer.createPromptModule();
+    questions(
+        {
+            // user will select which list they want to view
+            type: 'rawlist',
+            name: 'views',
+            message: 'Select an option to view data',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees'
+            ]
+            // console logs response obj in terminal
+        }).then((response) => {
+            // console.log(response);
+            if (response.views === 'View All Departments') {
+                db.query('SELECT * FROM departments', (error, departments) => {
+                    if (error) console.error(error);
+                    // displays query as a table in terminal
+                    console.table(departments);
+                    init();
+                })
+            }
+            if (response.views === 'View All Roles') {
+                db.query('SELECT * FROM job_titles', (error, job_titles) => {
+                    if (error) console.error(error);
+                    console.table(job_titles);
+                    init();
+                })
+            }
+            if (response.views === 'View All Employees') {
+                db.query('SELECT * FROM employees', (error, employees) => {
+                    if (error) console.error(error);
+                    console.table(employees);
+                    init();
+                })
+            }
+        })
+};
 
-//  TODO: add queries for :
-    // view all roles
-    // view all depts
-    // view all employees
+init();
+
